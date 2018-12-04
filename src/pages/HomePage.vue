2416bbdb5c38f6d30e6d332b9fa1b6f8c4d05f67
Dmitriy Vishnevskiy <d.vishnevsky@fast-report.com>
2018-12-04 14:29:02 +0300

    style(css): Убран лишний код с примера

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -9,21 +9,5 @@
 </script>
 
 <style>
-.product {
-  padding: 10px 0px;
-  border-bottom: 1px solid #eee;
-  width: 400px;
-}
 
-.title {
-  color: #312377;
-}
-
-.price {
-  float: right;
-}
-
-.product-container {
-  margin-bottom: 50px;
-}
 </style>
