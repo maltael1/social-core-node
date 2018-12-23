fe2a0d7dfface3042afe528eab16176277b45310
Dmitriy Vishnevskiy <d.vishnevsky@fast-report.com>
2018-12-23 23:38:23 +0300

    feat(view): Свёрстана форма входа

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,9 +1,11 @@
 <template>
-<form>
-    <span v-if="error">Ошибка входа</span>
+<form class="login-form">
     <input type="text" placeholder="Email" v-model="email">  
     <input type="password" placeholder="password" v-model="password">
-    <button class="button" v-on:click.prevent="get_token">Войти</button>
+    <div class="">
+        <button class="button" v-on:click.prevent="get_token">Войти</button>
+        <a href="#">Забыли пароль?</a>
+    </div>
 </form>  
 </template>
 
@@ -33,9 +35,46 @@ export default {
                 this.$emit('complite', response.data.token)
                 
             ))
-            .catch(err => { this.error = true})
+            .catch(err => { alert()})
         }
     }
 }
 </script>
+
+<style>
+    .login-form{
+        border: 1px solid #e6e6e6;
+        width: 258px;
+        height: 132px;
+        padding: 25px;
+        background: white;
+        border-radius: 3px;
+    }
+    .login-form input{
+        height: 19px;
+        width: 244px;
+        padding: 6px;
+        margin-bottom: 15px;
+        border-radius: 3px;
+        border: 1px solid #d3d9de;
+    }
+    .login-form a{
+        text-decoration: none;
+        width: 150px;
+        text-align: center;
+        display: inline-block;
+    }
+    .login-form button{
+        background-color: #655CC1;
+        border: none;
+        color: white;
+        padding: 10px 16px;
+        min-width: 100px;
+        border-radius: 3px;
+        cursor: pointer;
+    }
+    .login-form button:hover{
+        background-color: #6F65C5;
+    }
+</style>
 
---
+++ src/components/SocialLinkList.vue
@@ -0,0 +1,84 @@
+<template>
+<form class="social-link-list">
+    <span class="title">Войти через другой сервис</span>
+    <button class="button vk" >Вконтакте</button>
+    <button class="button facebook" >Facebook</button>
+    <button class="button ok" >Одноклассники</button>
+    <button class="button instagramm" >Instagramm</button>
+    <button class="button gmail">Gmail</button>
+</form>  
+</template>
+
+<script>
+const axios = require('axios');
+
+export default {
+    name: 'social-link-list',
+    data(){
+        return{
+        email: "",
+        password: "",
+        error: false,
+        api: ""
+        }
+    },
+    methods: {
+        
+    }
+}
+</script>
+
+<style>
+    .social-link-list{
+        border: 1px solid #e6e6e6;
+        width: 258px;
+        height: 283px;
+        padding: 25px;
+        background: white;
+        border-radius: 3px;
+    }
+    .social-link-list .button{
+        border: none;
+        color: white;
+        padding: 10px 16px;
+        width: 260px;
+        margin-bottom: 15px;
+        border-radius: 3px;
+        cursor: pointer;
+    }
+    .vk{
+        background-color: rgb(101, 154, 197);
+    }
+    .facebook{
+        background-color: rgb(51, 85, 199);
+    }
+    .ok{
+        background-color: rgb(252, 116, 38);
+    }
+    .instagramm{
+        background-color: rgb(238, 107, 179);
+    }
+    .gmail{
+        background-color: rgb(233, 55, 55);
+    }
+    .vk:hover{
+        background-color: rgb(120, 165, 202);
+    }
+    .facebook:hover{
+        background-color: rgb(72, 103, 204);
+    }
+    .ok:hover{
+        background-color: rgb(250, 100, 14);
+    }
+    .instagramm:hover{
+        background-color: rgb(231, 127, 184);
+    }
+    .gmail:hover{
+        background-color: rgb(233, 84, 84);
+    }
+    .social-link-list .title{
+        font-size: 22px;
+        padding-bottom: 10px;
+        display: block;
+    }
+</style>
